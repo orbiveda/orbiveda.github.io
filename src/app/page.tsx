9f5046acae0c8d782e7642f14a43646aca004367
Harsh Mukharji <harshmukharji10@gmail.com>
2025-07-14 12:55:06 +0000

    Navratri created different pages and not everything on the homepage brin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 import HeroSection from '@/components/sections/hero';
-import AboutSummary from '@/components/sections/about-summary';
-import ProductHighlights from '@/components/sections/product-highlights';
-import Testimonials from '@/components/sections/testimonials';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <AboutSummary />
-      <ProductHighlights />
-      <Testimonials />
     </>
   );
 }
