5e21a7d65a79ead8530dcf28430cc14f9126e33b
Harsh Mukharji <harshmukharji10@gmail.com>
2025-07-14 16:06:29 +0000

    Now the photo image lords but the men images not load the also the websi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={cn("font-sans antialiased", poppins.variable)}>
         {children}
         <Toaster />
