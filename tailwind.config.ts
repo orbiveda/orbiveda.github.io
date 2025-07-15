925fc26ad9d692f8fc5bc59a829261a1b238cfad
Tiger Gaming <harshgamer3556940@gmail.com>
2025-07-15 16:41:51 +0000

    the web is not as in preview have different on live

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,7 @@ import { fontFamily } from "tailwindcss/defaultTheme"
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
