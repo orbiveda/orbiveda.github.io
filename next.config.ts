3fc2f5b17632fd3689ec5097bf83f544fbc1237c
Tiger Gaming <harshgamer3556940@gmail.com>
2025-07-18 11:36:39 +0000

    can u see why image not loading

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '' : undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
